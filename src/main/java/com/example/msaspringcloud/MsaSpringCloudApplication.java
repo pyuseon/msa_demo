296a607d3c74feb112827e80dc991b3795142b7f
yuseonpark <pyuseon@gmail.com>
2025-01-04 19:33:44 +0900

    feat: setting for tutorial

--- src/main/java/com/example/msaspringcloud/MsaSpringCloudApplication.java
+++ src/main/java/com/example/msaspringcloud/MsaSpringCloudApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsaSpringCloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsaSpringCloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsaSpringCloudApplication.class, args);
+    }
 
 }
